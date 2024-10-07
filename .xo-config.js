bd1a4205c08b0b6bd81f112675425a51298dea8a
JC Franco <jfranco@esri.com>
2024-10-07 09:44:57 -0700

    update rules

--- .xo-config.js
+++ .xo-config.js
@@ -2,9 +2,25 @@ module.exports = {
   prettier: true,
   space: true,
   globals: [
-    // jest
+    // chai/mocha
+    "after",
+    "afterEach",
+    "before",
+    "beforeEach",
     "describe",
-    "it",
     "expect",
-  ]
+    "it",
+  ],
+  rules: {
+    "import/extensions": "off",
+    "unicorn/no-array-reduce": "off",
+    "unicorn/prevent-abbreviations": [
+      "error",
+      {
+        "allowList": {
+          "dst": true,
+        }
+      }
+    ]
+  }
 };
